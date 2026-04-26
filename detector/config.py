d43ecde4b15c13e7e4936711a7bb00aa7e0d2b86
Joseph Joshua <calebayuba222@gmail.com>
2026-04-26 13:06:25 +0100

    feat: read web hook url from env file

--- detector/config.py
+++ detector/config.py
@@ -7,6 +7,8 @@ CONFIG_PATH = os.path.join(os.path.dirname(__file__), "config.yaml")
 def load_config():
     with open(CONFIG_PATH, "r") as f:
         config = yaml.safe_load(f)
+
+    config["slack"]["webhook_url"] = os.getenv("WEB_HOOK_URL")
     return config
 
 
