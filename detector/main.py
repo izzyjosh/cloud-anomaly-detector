fcfd218a3fbb001f078a589813350c7757b72d9a
Joseph Joshua <calebayuba222@gmail.com>
2026-04-26 15:24:45 +0100

    fix: start monitor in a separate thread

--- detector/main.py
+++ detector/main.py
@@ -6,5 +6,8 @@ from dashboard import start_dashboard
 if __name__ == "__main__":
     unban_thread = threading.Thread(target=start_unban_scheduler, daemon=True)
     unban_thread.start()
-    start_monitoring()
+
+    monitor_thread = threading.Thread(target=start_monitoring, daemon=True)
+    monitor_thread.start()
+
     start_dashboard()
